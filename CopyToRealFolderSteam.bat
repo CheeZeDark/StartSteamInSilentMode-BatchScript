4db8b4875a12086f00dc1686560bd606874f623f
CheeZe Dark(Rikko Matsumato) <110032482+CheeZeDark@users.noreply.github.com>
2025-09-16 14:14:17 +0300

    Create CopyToRealFolderSteam.bat

---
+++ CopyToRealFolderSteam.bat
@@ -0,0 +1,5 @@
+@echo off
+
+xcopy startsteaminsilentmode.bat "C:\Program Files(x86)\Steam\startsteaminsilentmode.bat"
+echo Successfully Copied... if not copied, pls make sure what you downloaded Steam in right folder and try again launch this BatchScript!!! Created By CheeZeDark(Rikko Matsumato)
+exit
